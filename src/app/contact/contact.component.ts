fdc28c4851e404ffb58dedf12ed38e9ff24ab782
ricardomartinez9568 <rm3@mscpsharks.org>
2018-03-17 14:13:42 -0700

    Contact Form

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {NgForm, NgModel} from '@angular/forms';
+
+class NewForm {
+  FirstName: string;
+  LastName: string;
+  email: string;
+  phone: string;
+  message: string;
+}
+
 
 @Component({
   selector: 'app-contact',
@@ -6,10 +17,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
+  public newForm: NewForm;
 
-  constructor() { }
-
+  constructor(private http: HttpClient) {
+    this.newForm = new NewForm();
+   }
+   sendMessage() {
+     console.log(this.newForm);
+     console.log('button clicked');
+   }
   ngOnInit() {
+  this.newForm = new NewForm();
   }
 
+  onSubmit() {
+    console.log('submit');
+    this.http.post('http://localhost:3000' + '/contact', this.newForm)
+    .subscribe(
+      result => {
+        console.log(result);
+      },
+      err => {
+        console.log(err);
+      });
+  }
 }
