54b34b8f59e944baff1b9191e6c0ef6eb27baba1
ricardomartinez9568 <rm3@mscpsharks.org>
2018-03-24 18:06:36 -0700

    Working blog section

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -1,4 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
+import { BlogService } from '../_services/Blog/blog.service';
+import { baseUrl } from '../_services/httpBaseUrl/httpBaseUrl';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+
+class NewBlog {
+  Author: String;
+  Content: String;
+  ID: Number;
+  timeStamp: Date;
+}
+interface NewBlogInterface {
+  Author: String;
+  Content: String;
+  ID: Number;
+  timeStamp: Date;
+}
+
 
 @Component({
   selector: 'app-blog',
@@ -6,10 +26,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./blog.component.css']
 })
 export class BlogComponent implements OnInit {
+  public blog: NewBlog;
+  public headerID;
+  public httpHead;
 
-  constructor() { }
 
-  ngOnInit() {
+  constructor(private route: ActivatedRoute, private router: Router, private http: HttpClient) {
+    this.route.params.subscribe((params) => {
+      this.headerID = params.id;
+    });
   }
 
-}
+  ngOnInit() {
+    this.blog = new NewBlog;
+    this.http.get(baseUrl + '/blog', {
+      headers: new HttpHeaders().set('headerID', this.headerID)
+    })
+      .subscribe((data: NewBlogInterface) => {
+        this.blog = data;
+        console.log(this.blog);
+      });
+
+    }
+  }
